6d50dbb3b53bd5e00d2a1245555faec8f7d926a8
Roy Ratcliffe <roy@ratcliffe.me>
2024-04-28 06:04:44 +0100

    refactor(callbacks): demo script

--- scripts/callbacks.lua
+++ scripts/callbacks.lua
@@ -1,22 +1,3 @@
-local env = {}
-
---- Sets a new screen.
--- Updates the `Screen.image` variable bound to the image control.
--- @tparam ?string screen New screen.
-function env:set_screen(screen)
-  if screen then
-    self.screen = screen
-    gre.set_value("Screen.image", self.screens[screen].image)
-  end
-end
-
---- Answers new screen for given transition.
--- @tparam ?string transition Transition to search for.
--- @treturn ?string New screen for transition.
-function env:screen_for(transition)
-  return transition and self.screens[self.screen][transition]
-end
-
 --- Initialises application.
 -- Loads the demo environment.
 -- @tparam gre#context mapargs GRE arguments.
@@ -36,7 +17,5 @@ function key_down_cb(mapargs)
   local key = mapargs.context_event_data.key
   local transition = env.keys[key]
   print("key_down: " .. (transition or key))
-  if transition then
-    env:set_screen(env:screen_for(transition))
-  end
+  env:set_screen(env:screen_for(transition))
 end
