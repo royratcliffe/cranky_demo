b9e3c0bfd5a26ba978a8d899e4db959370527fba
Roy Ratcliffe <roy@ratcliffe.me>
2024-05-07 15:15:05 +0100

    refactor: vlo, vhi

--- scripts/bq25790.lua
+++ scripts/bq25790.lua
@@ -1,11 +1,13 @@
 local POWER = require "power"
 
+local vlo, vhi = 5.6, 8.4
+
 gre.timer_set_interval(function()
   gre.thread_create(function()
     local succeeded, powersupply = pcall(POWER.supply, "battery", "charger")
     if succeeded then
       local voltage = powersupply.battery.POWER_SUPPLY_VOLTAGE_NOW / 1e6
-      set_battery_percent(100 * (voltage - 5.4) / (8.4 - 5.4))
+      set_battery_percent(100 * (voltage - vlo) / (vhi - vlo))
     end
   end)
 end, 2000)
